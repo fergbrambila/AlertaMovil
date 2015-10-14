d08c5b8c11c8977d83e60998c43255e3e41e3770
Fernando Garza <fergbrambila@gmail.com>
2015-10-14 17:04:21 -0500

    Funciona el link para sign up, y el boton verifica los campos, solo falta verificar dentro de alguna base de datos

--- app/src/main/java/com/example/moviles/alertamovil/SignupActivity.java
+++ app/src/main/java/com/example/moviles/alertamovil/SignupActivity.java
@@ -1,13 +1,32 @@
 package com.example.moviles.alertamovil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
+    private static TextView _link_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        onLinkListener();
     }
+
+    public void onLinkListener(){
+        _link_login = (TextView)findViewById(R.id.link_login);
+        _link_login.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        finish();
+                    }
+                }
+        );
+    }
+
 }
